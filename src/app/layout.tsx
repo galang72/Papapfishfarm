908852389b4471f343e276d8f3e05530ea532102
galang72 <galang72@gmail.com>
2026-09-04 18:31:58 +0700

    feat: fix google site verification token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'Papap Fish Farm - Sistem Budidaya & Keuangan Ikan Air Tawar',
   description: 'Aplikasi manajemen dan pencatatan budidaya ikan air tawar, keuangan, panen, kematian ikan, analisis FCR, ROI, dan laba rugi.',
   verification: {
-    google: "jIOLsGAQhPF3VG-jdPzGdrWwgYTSv9cOyN9xG2vodu0",
+    google: "jIOLsGAQhPF3VG-jdPzGdrWwgYTSv9cOyN9xG2vodu0"
   },
 };
 
